8158c52920268b7f42a47af5df20879277e7c9d1
lebronble <wangruicheng7471@163.com>
2026-04-02 10:30:00 +0800

    Add files via upload

---
+++ tools.py
@@ -0,0 +1,57 @@
+import torch 
+from torch import nn
+
+import math
+
+class PositionwiseFeedForward(nn.Module):
+
+    def __init__(self, d_model, hidden, drop_prob=0.1):
+        super(PositionwiseFeedForward, self).__init__()
+        self.linear1 = nn.Linear(d_model, hidden)
+        self.linear2 = nn.Linear(hidden, d_model)
+        self.relu = nn.ReLU()
+        self.dropout = nn.Dropout(p=drop_prob)
+
+    def forward(self, x):
+        x = self.linear1(x)
+        x = self.relu(x)
+        x = self.dropout(x)
+        x = self.linear2(x)
+        return x
+
+class LayerNorm(nn.Module):
+    def __init__(self, d_model, eps=1e-12):
+        super(LayerNorm, self).__init__()
+        self.gamma = nn.Parameter(torch.ones(d_model))
+        self.beta = nn.Parameter(torch.zeros(d_model))
+        self.eps = eps
+
+    def forward(self, x):
+        mean = x.mean(-1, keepdim=True)
+        var = x.var(-1, unbiased=False, keepdim=True)
+
+        out = (x - mean) / torch.sqrt(var + self.eps)
+        out = self.gamma * out + self.beta
+        return out
+
+
+class ScaleDotProductAttention(nn.Module):
+
+    def __init__(self):
+        super(ScaleDotProductAttention, self).__init__()
+        self.softmax = nn.Softmax(dim=-1)
+
+    def forward(self, q, k, v, mask=None, e=1e-12):
+        batch_size, head, length, d_tensor = k.size()
+        k_t = k.transpose(2, 3)  
+
+        score = (q @ k_t) / math.sqrt(d_tensor) 
+
+        if mask is not None:
+            score = score.masked_fill(mask == 0, -10000)
+
+        score = self.softmax(score)
+
+        v = score @ v
+
+        return v
